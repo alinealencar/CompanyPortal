182499c7dd198cc217949e40bcd3a89371c2b4b5
Aline <aline.nevesalencar@georgebrown.ca>
2017-10-16 16:04:29 -0400

    Connect to database

---
+++ src/Database/DatabaseAccess.java
@@ -0,0 +1,33 @@
+package Database;
+/****************************************************************************************************
+* Description: DatabaseAccess - Example provides access to database
+****************************************************************************************************/
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseAccess {
+	private String username = "root";
+	private String password = "admin";
+	private String database = "COMP3095";
+	
+	private static Connection connect = null;
+	  
+	public Connection connectDataBase() throws Exception {
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+//			connect = DriverManager
+//		          .getConnection("jdbc:mysql://localhost:3306/" + database + "?"
+//		              + "user=" + username + "&password=" + password);
+			connect = DriverManager
+			          .getConnection("jdbc:mysql://localhost:3306/" + database,
+			              username, password);
+	      return connect;
+	    } catch (Exception e) {
+	      throw e;
+	    } 
+	  }
+	
+	public String getFoo() { return "foo"; }
+}
