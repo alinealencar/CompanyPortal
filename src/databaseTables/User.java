fd21fc4ab2324abd10fe05da3eace378bcaf2e63
Princess Ilasin <princess.ilasin@georgebrown.ca>
2017-10-18 18:29:06 -0400

    Update User.java

--- src/databaseTables/User.java
+++ src/databaseTables/User.java
@@ -2,28 +2,59 @@ package databaseTables;
 
 public class User {
 
-	private String username;
-	private String password;
-	
-	private User (String username, String password){
+	private String firstname, lastname, email, role, username, password;
+	private int id;
+	public User(String firstname, String lastname, String email, String role, String username, String password,
+			int id) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.role = role;
 		this.username = username;
 		this.password = password;
+		this.id = id;
 	}
-
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
