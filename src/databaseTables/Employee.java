34b2b36a56171bc4154bda2253c14a4286737df9
princessilasin <princess.ilasin@georgebrown.ca>
2017-10-18 18:52:38 -0400

    add

--- src/databaseTables/Employee.java
+++ src/databaseTables/Employee.java
@@ -2,64 +2,60 @@ package databaseTables;
 
 public class Employee {
 
-	private String fName, lName, employeeNo, email, role;
-	private int hireYear;
-	
-	public Employee(String fName, String lName, String employeeNo, String email, String role, int hireYear) {
-        this.fName = fName;
-        this.lName = lName;
-        this.employeeNo = employeeNo;
-        this.email = email;
-        this.role = role;
-        this.hireYear = hireYear;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getEmployeeNo() {
-        return employeeNo;
-    }
-
-    public void setEmployeeNo(String employeeNo) {
-        this.employeeNo = employeeNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String jobPos) {
-        this.role = jobPos;
-    }
-
-    public int getHireYear() {
-        return hireYear;
-    }
-
-    public void setHireYear(int hireYear) {
-        this.hireYear = hireYear;
-    }
+	private String firstname, lastname, emp_no, hire_year, job_position;
+	private int emp_id, dept_id;
+	public Employee(String firstname, String lastname, String emp_no, String hire_year, String job_position, int emp_id,
+			int dept_id) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.emp_no = emp_no;
+		this.hire_year = hire_year;
+		this.job_position = job_position;
+		this.emp_id = emp_id;
+		this.dept_id = dept_id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmp_no() {
+		return emp_no;
+	}
+	public void setEmp_no(String emp_no) {
+		this.emp_no = emp_no;
+	}
+	public String getHire_year() {
+		return hire_year;
+	}
+	public void setHire_year(String hire_year) {
+		this.hire_year = hire_year;
+	}
+	public String getJob_position() {
+		return job_position;
+	}
+	public void setJob_position(String job_position) {
+		this.job_position = job_position;
+	}
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public int getDept_id() {
+		return dept_id;
+	}
+	public void setDept_id(int dept_id) {
+		this.dept_id = dept_id;
+	}
 	
 }
