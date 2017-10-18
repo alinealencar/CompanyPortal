92c6d4cb73be0bea87bd18b91594341d437518d3
Princess Ilasin <princess.ilasin@georgebrown.ca>
2017-10-18 16:02:52 -0400

    Update Group.java

--- src/helper/Group.java
+++ src/helper/Group.java
@@ -5,25 +5,87 @@ public class Group {
 	
 	private String deptName;
     private String groupName;
+    private String member1;
+    private String member2;
+    private String member3;
+    private String member4;
+    private String member5;
+    private String member6;
+    
+	public Group(String deptName, String groupName, String member1, String member2, String member3, String member4,
+			String member5, String member6) {
+		super();
+		this.deptName = deptName;
+		this.groupName = groupName;
+		this.member1 = member1;
+		this.member2 = member2;
+		this.member3 = member3;
+		this.member4 = member4;
+		this.member5 = member5;
+		this.member6 = member6;
+	}
 
-    public Group(String deptName, String groupName) {
-        this.deptName = deptName;
-        this.groupName = groupName;
-    }
+	public String getDeptName() {
+		return deptName;
+	}
 
-    public String getDeptName() {
-        return deptName;
-    }
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
 
-    public void setDeptName(String getDeptName) {
-        this.deptName = getDeptName;
-    }
+	public String getGroupName() {
+		return groupName;
+	}
 
-    public String getGroupName() {
-        return groupName;
-    }
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
 
-    public void setGroupNamae(String groupName) {
-        this.groupName = groupName;
-    } 
+	public String getMember1() {
+		return member1;
+	}
+
+	public void setMember1(String member1) {
+		this.member1 = member1;
+	}
+
+	public String getMember2() {
+		return member2;
+	}
+
+	public void setMember2(String member2) {
+		this.member2 = member2;
+	}
+
+	public String getMember3() {
+		return member3;
+	}
+
+	public void setMember3(String member3) {
+		this.member3 = member3;
+	}
+
+	public String getMember4() {
+		return member4;
+	}
+
+	public void setMember4(String member4) {
+		this.member4 = member4;
+	}
+
+	public String getMember5() {
+		return member5;
+	}
+
+	public void setMember5(String member5) {
+		this.member5 = member5;
+	}
+
+	public String getMember6() {
+		return member6;
+	}
+
+	public void setMember6(String member6) {
+		this.member6 = member6;
+	}
 }
