0ad5adb0a43d331268efc7892acf31a7f6c12e2b
alyssavillacete <alyssavillacete@yahoo.com>
2017-10-19 22:23:19 -0400

    Edit CookieUtilities

--- src/helper/CookieUtilities.java
+++ src/helper/CookieUtilities.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 public class CookieUtilities {
 	
 	public static String getCookieValue(HttpServletRequest request, String cookieName, String defaultValue) {
-				Cookie[] cookies = request.getCookies();
-				if( cookies != null ) {
-					for( Cookie cookie : cookies ){
-						if(cookieName.equals(cookie.getName())){
-							return (cookie.getValue());
-						}
+		Cookie[] cookies = request.getCookies();
+			if( cookies != null ) {
+				for( Cookie cookie : cookies ){
+					if(cookieName.equals(cookie.getName())){
+						return (cookie.getValue());
 					}
+				}
 			}
 			return defaultValue;
 		}
